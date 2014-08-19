db00b0174355c1d64b3b4ff72667f4c027fb5964
xtrios <xtrios@gmail.com>
2014-08-19 23:34:24 +0800

    Assignment2
    makeCacheMatrix & cacheSolve

---
+++ cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
